da1769d3eee71e0b3b4589541857a7a90be92fc7
Luke Fernandez <lukejohnfernandez@gmail.com>
2016-10-06 21:47:47 -0400

    set up quick web view of class website

---
+++ main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var electron = require('electron');
+var app = electron.app;
+var BrowserWindow = electron.BrowserWindow;
+
+var mainWindow = null;
+
+app.on('ready', function() {
+    mainWindow = new BrowserWindow({
+        height: 800,
+        width: 1000
+    });
+
+    mainWindow.loadURL('file://' + __dirname + '/app/index.html');
+});
