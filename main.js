d67a0ad0607a7a1564a21b78a1d8a4fa2e6aa7dd
Luke Fernandez <lukejohnfernandez@gmail.com>
2016-10-23 11:19:57 -0400

    updated package.json, added in app.js called by index.html

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
 'use strict';
 
-var electron = require('electron');
-var app = electron.app;
-var BrowserWindow = electron.BrowserWindow;
+const {app, BrowserWindow} = require('electron'); // accomplishes the same as the following
+	/* 
+		Accomplishes the same as the following:
+			var electron = require('electron');
+			var app = electron.app;
+			var BrowserWindow = electron.BrowserWindow; 
+	*/
 
 var mainWindow = null;
 
 app.on('ready', function() {
     mainWindow = new BrowserWindow({
-        height: 800,
-        width: 1000
+        height: 500,
+        width: 800
     });
 
+    // Loads the index.html file via navigation from the current directory
     mainWindow.loadURL('file://' + __dirname + '/app/index.html');
 });
