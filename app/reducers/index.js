a4747c12c3a06b8b7ff5bfeb315eb17d442eebce
dongy7 <dongy7@gmail.com>
2016-10-31 11:56:02 -0400

    add navigation reducer

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import { routerReducer as routing } from 'react-router-redux';
 import videos from './videos';
+import navigation from './navigation';
 
 const rootReducer = combineReducers({
   videos,
+  navigation,
   routing,
 });
 
---
+++ app/reducers/navigation.js
@@ -0,0 +1,10 @@
+const initialState = {
+  open: false,
+};
+
+export default function navigation(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
