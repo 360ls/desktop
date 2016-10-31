93142b9690744215762194aaed05b938520bbba7
dongy7 <dongy7@gmail.com>
2016-10-31 12:07:24 -0400

    add navigation actions

---
+++ app/actions/navigation.js
@@ -0,0 +1,7 @@
+export const TOGGLE_NAV = 'TOGGLE_NAV';
+
+export function toggle() {
+  return {
+    type: TOGGLE_NAV,
+  };
+}
--- app/reducers/navigation.js
+++ app/reducers/navigation.js
@@ -1,9 +1,15 @@
+import { TOGGLE_NAV } from '../actions/navigation';
+
 const initialState = {
   open: false,
 };
 
 export default function navigation(state = initialState, action) {
   switch (action.type) {
+    case TOGGLE_NAV:
+      return Object.assign({}, state, {
+        open: !state.open,
+      });
     default:
       return state;
   }
