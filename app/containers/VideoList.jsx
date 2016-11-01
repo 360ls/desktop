965c4ccafbcba611c428cddc6ce508c2278bed98
dongy7 <dongy7@gmail.com>
2016-11-01 16:05:42 -0400

    filter video list by active visibility filter

--- app/containers/VideoList.jsx
+++ app/containers/VideoList.jsx
@@ -2,8 +2,21 @@ import { connect } from 'react-redux';
 import VideoTable from '../components/VideoTable';
 import { switchVideo } from '../actions/videos';
 
+const getVisibleVideos = (videos, filter) => {
+  switch (filter) {
+    case 'All':
+      return videos;
+    case 'Uploaded':
+      return videos.filter(video => video.status);
+    case 'Flagged':
+      return videos.filter(video => video.flag);
+    default:
+      return videos;
+  }
+};
+
 const mapStateToProps = state => ({
-  videos: state.videos,
+  videos: getVisibleVideos(state.videos, state.visibilityFilter),
 });
 
 const mapDispatchToProps = dispatch => ({
