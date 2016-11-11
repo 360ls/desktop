25fed174c832ab5ce969f716fda16ee7a10b0ef1
dongy7 <dongy7@gmail.com>
2016-11-10 22:20:00 -0500

    hook toggle action creator to button onclick

--- app/components/RecordButton.jsx
+++ app/components/RecordButton.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 
-const RecordButton = () => (
+const RecordButton = ({ toggleStream }) => (
   <div>
     <RaisedButton
       label="Record"
       primary
       fullWidth
+      onClick={() => toggleStream()}
     />
   </div>
 );
+
+RecordButton.propTypes = {
+  toggleStream: PropTypes.func.isRequired,
+};
 
 export default RecordButton;
--- app/reducers/index.js
+++ app/reducers/index.js
@@ -4,7 +4,7 @@ import videos from './videos';
 import createNavigation from './navigation';
 import createPlayer from './player';
 import visibilityFilter from './visibilityFilter';
-import live from './live';
+import createLive from './live';
 
 const rootReducer = combineReducers({
   videos,
@@ -12,7 +12,7 @@ const rootReducer = combineReducers({
   player: createPlayer(),
   routing,
   visibilityFilter,
-  live,
+  live: createLive(),
 });
 
 export default rootReducer;
