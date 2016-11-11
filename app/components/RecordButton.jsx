cc7bbba18a74cd59f1e75cea18af5a3f5b4211b6
dongy7 <dongy7@gmail.com>
2016-11-10 23:39:16 -0500

    render record button conditionally

--- app/components/RecordButton.jsx
+++ app/components/RecordButton.jsx
@@ -1,16 +1,31 @@
 import React, { PropTypes } from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 
-const RecordButton = ({ toggleStream }) => (
-  <div>
+const RecordButton = ({ toggleStream, isStreaming }) => {
+  const startButton = (
     <RaisedButton
       label="Record"
       primary
       fullWidth
       onClick={() => toggleStream()}
     />
-  </div>
-);
+  );
+
+  const stopButton = (
+    <RaisedButton
+      label="Stop"
+      secondary
+      fullWidth
+      onClick={() => toggleStream()}
+    />
+  );
+
+  return (
+    <div>
+      {isStreaming ? stopButton : startButton}
+    </div>
+  );
+};
 
 RecordButton.propTypes = {
   toggleStream: PropTypes.func.isRequired,
