442778596fa5db38ad21796dc6857de6238b6ce7
dongy7 <dongy7@gmail.com>
2016-11-13 14:24:59 -0500

    add basic function to save video

--- app/services/feed.py
+++ app/services/feed.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import sys
+import signal, os
+import argparse
 
 try:
     import pip
@@ -12,17 +14,33 @@ try:
 except:
     raise Exception('OpenCV is not installed')
 
-cap = cv2.VideoCapture('dist/sample.avi')
+def handler(signum, frame):
+    # When everything done, release the capture
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+    sys.exit(0)
 
-while(cap.isOpened()):
+signal.signal(signal.SIGINT, handler)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-f')
+args = parser.parse_args()
+
+dest = args.f
+
+cap = cv2.VideoCapture(0)
+out = cv2.VideoWriter(dest, -1, 20.0, (640, 480));
+
+while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
+
+    frame = cv2.resize(frame, (640, 480))
+
+    out.write(frame);
 
     # Display the resulting frame
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
