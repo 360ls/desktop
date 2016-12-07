cd317900025cb7dd63bdc97535f84653975e3620
Luke Fernandez <lukejohnfernandez@gmail.com>
2016-12-06 19:45:38 -0500

    added dashboard jumbotron

--- app/pages/DashboardPage.jsx
+++ app/pages/DashboardPage.jsx
@@ -2,20 +2,23 @@ import React from 'react';
 
 const DashboardPage = () => (
   <div className="container">
-    <h1>
-      Welcome to the 360 Life Stream Desktop Application.
-    </h1>
-    <h2>
-      This application is meant for police officers to use in their cars,
-      and it provides the following functionality:
-    </h2>
-    <h3>
-      <ul>
-        <li>
-          Stitching and streaming from two camera feeds positioned at 90-degrees from one another.
-        </li>
-      </ul>
-    </h3>
+    <div className="jumbotron">
+      <h1>
+        Welcome to the 360 Life Stream Desktop Application.
+      </h1>
+      <h2>
+        This application is meant for police officers to use in their cars,
+        and it provides the following functionality:
+      </h2>
+      <h3>
+        <ol>
+          <li> Display of one camera feed locally. </li>
+          <li> Recording of one camera feed locally with upload to server. </li>
+          <li> Streaming from one camera feed to hosted Wowza web service for later consumption. </li>
+          <li> DVR playback of videos saved on the server post-recording. </li>
+        </ol>
+      </h3>
+    </div>
   </div>
 );
 
