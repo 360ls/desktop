4d79d49eb6e542f43636c6232c98953ffc1b28d3
dongy7 <dongy7@gmail.com>
2016-12-09 13:16:29 -0500

    clean up stitcher script

--- app/services/stitcher.py
+++ app/services/stitcher.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 
+"""
+Script for streaming a camera feed
+"""
+
 import sys
-import signal, os
+import signal
 import argparse
 import subprocess
 
@@ -11,6 +15,9 @@ except:
     raise Exception('OpenCV is not installed')
 
 def parse_args():
+    """
+    Parses command line arguments
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', default='')
     parser.add_argument('-i', type=int, default=0)
@@ -24,57 +31,65 @@ def parse_args():
     return parser.parse_args()
 
 def check_index(index):
+    """
+    Checks if given index is valid
+    """
     sample_cap = cv2.VideoCapture(index)
     frame = sample_cap.grab()
     sample_cap.release()
-    if frame:
-        return True
-    else:
-        return False
+    return frame
 
-def handler(signum, frame):
-    # When everything done, release the capture
-    cap.release()
-    out.release()
-    cv2.destroyAllWindows()
-    sys.exit(0)
+def main():
+    """
+    Parses command line arguments and starts the stitcher
+    """
+    args = parse_args()
+    dest = args.f
+    index = args.i
+    height = args.height
+    width = args.width
 
-signal.signal(signal.SIGINT, handler)
-signal.signal(signal.SIGTERM, handler)
+    cap = cv2.VideoCapture(index)
+    codec = cv2.cv.CV_FOURCC('m', 'p', '4', 'v')
+    out = cv2.VideoWriter(dest, codec, 20.0, (width, height))
+    dimensions = str(width) + 'x' + str(height)
 
-ext = ''
-args = parse_args()
+    def handler(signum, frame): # pylint: disable=unused-argument
+        """
+        Interrupt handler
+        """
+        # When everything done, release the capture
+        cap.release()
+        out.release()
+        cv2.destroyAllWindows()
+        sys.exit(0)
 
-dest = args.f + ext
-index = args.i
-height = args.height
-width = args.width
-
-cap = cv2.VideoCapture(index)
-codec = cv2.cv.CV_FOURCC('m', 'p', '4', 'v')
-out = cv2.VideoWriter(dest, codec, 20.0, (width, height));
-dimensions = str(width) + 'x' + str(height)
-
-if args.stream:
-    proc = subprocess.Popen([
-        'ffmpeg', '-y', '-f', 'rawvideo',
-        '-s', dimensions, '-pix_fmt', 'bgr24', '-i','pipe:0','-vcodec',
-        'libx264','-pix_fmt','uyvy422','-r','28','-an', '-f','flv',
-        args.url], stdin=subprocess.PIPE)
-
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-
-    frame = cv2.resize(frame, (width, height))
-
-    if not args.preview:
-        out.write(frame);
+    signal.signal(signal.SIGINT, handler)
+    signal.signal(signal.SIGTERM, handler)
 
     if args.stream:
-        proc.stdin.write(frame.tostring())
+        proc = subprocess.Popen([
+            'ffmpeg', '-y', '-f', 'rawvideo',
+            '-s', dimensions, '-pix_fmt', 'bgr24', '-i', 'pipe:0', '-vcodec',
+            'libx264', '-pix_fmt', 'uyvy422', '-r', '28', '-an', '-f', 'flv',
+            args.url], stdin=subprocess.PIPE)
 
-    # Display the resulting frame
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    while True:
+        # Capture frame-by-frame
+        _, frame = cap.read()
+
+        frame = cv2.resize(frame, (width, height))
+
+        if not args.preview:
+            out.write(frame)
+
+        if args.stream:
+            proc.stdin.write(frame.tostring())
+
+        # Display the resulting frame
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+if __name__ == "__main__":
+    main()
