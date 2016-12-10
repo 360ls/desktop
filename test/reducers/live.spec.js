3b8a9ea3f3e38c81c795e14ac07f8df6629cb05c
dongy7 <dongy7@gmail.com>
2016-12-10 14:44:30 -0500

    add missing toggle tests

--- test/reducers/live.spec.js
+++ test/reducers/live.spec.js
@@ -1,5 +1,9 @@
 import live from '../../app/reducers/live';
-import { TOGGLE_STREAM } from '../../app/actions/live';
+import {
+  TOGGLE_BROADCAST,
+  TOGGLE_PREVIEW,
+  TOGGLE_STREAM,
+} from '../../app/actions/live';
 
 describe('reducers', () => {
   describe('live', () => {
@@ -30,6 +34,54 @@ describe('reducers', () => {
         recording: true,
       }, {
         type: TOGGLE_STREAM,
+      })).toEqual({
+        recording: false,
+        broadcast: false,
+        preview: false,
+      });
+    });
+
+    it('should handle toggle preview from close to open', () => {
+      expect(live()({
+        preview: false,
+      }, {
+        type: TOGGLE_PREVIEW,
+      })).toEqual({
+        recording: false,
+        broadcast: false,
+        preview: true,
+      });
+    });
+
+    it('should handle toggle preview from open to close', () => {
+      expect(live()({
+        preview: true,
+      }, {
+        type: TOGGLE_PREVIEW,
+      })).toEqual({
+        recording: false,
+        broadcast: false,
+        preview: false,
+      });
+    });
+
+    it('should handle toggle broadcast from close to open', () => {
+      expect(live()({
+        broadcast: false,
+      }, {
+        type: TOGGLE_BROADCAST,
+      })).toEqual({
+        recording: false,
+        broadcast: true,
+        preview: false,
+      });
+    });
+
+    it('should handle toggle broadcast from open to close', () => {
+      expect(live()({
+        broadcast: true,
+      }, {
+        type: TOGGLE_BROADCAST,
       })).toEqual({
         recording: false,
         broadcast: false,
