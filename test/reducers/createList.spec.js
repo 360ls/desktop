192c165371c9cf35eea384c73e6579e4ea275c3a
dongy7 <dongy7@gmail.com>
2016-12-10 16:26:51 -0500

    add createList reducer tests

--- test/reducers/createList.spec.js
+++ test/reducers/createList.spec.js
@@ -1,6 +1,8 @@
 import createList from '../../app/reducers/createList';
 
 const filter = 'All';
+const flaggedFilter = 'Flagged';
+const videos = ['foo', 'bar'];
 
 describe('reducers', () => {
   describe('createList', () => {
@@ -11,6 +13,79 @@ describe('reducers', () => {
         ids: [],
         isFetching: false,
         errorMessage: null,
+      });
+    });
+
+    it('should handle fetch success if filter matches', () => {
+      expect(createList(filter)(undefined, {
+        type: 'FETCH_VIDEOS_SUCCESS',
+        filter,
+        response: {
+          result: videos,
+        },
+      })).toEqual({
+        ids: videos,
+        isFetching: false,
+        errorMessage: null,
+      });
+    });
+
+    it('should handle fetch success if filter does not match', () => {
+      expect(createList(filter)(undefined, {
+        type: 'FETCH_VIDEOS_SUCCESS',
+        filter: flaggedFilter,
+        response: {
+          result: videos,
+        },
+      })).toEqual({
+        ids: [],
+        isFetching: false,
+        errorMessage: null,
+      });
+    });
+
+    it('should handle fetch request if filter matches', () => {
+      expect(createList(filter)(undefined, {
+        type: 'FETCH_VIDEOS_REQUEST',
+        filter,
+      })).toEqual({
+        ids: [],
+        isFetching: true,
+        errorMessage: null,
+      });
+    });
+
+    it('should handle fetch request if filter does not match', () => {
+      expect(createList(filter)(undefined, {
+        type: 'FETCH_VIDEOS_REQUEST',
+        filter: flaggedFilter,
+      })).toEqual({
+        ids: [],
+        isFetching: false,
+        errorMessage: null,
+      });
+    });
+
+    it('should handle fetch success if filter does not match', () => {
+      expect(createList(filter)(undefined, {
+        type: 'FETCH_VIDEOS_SUCCESS',
+        filter: flaggedFilter,
+      })).toEqual({
+        ids: [],
+        isFetching: false,
+        errorMessage: null,
+      });
+    });
+
+    it('should handle fetch failure', () => {
+      expect(createList(filter)(undefined, {
+        type: 'FETCH_VIDEOS_FAILURE',
+        filter,
+        message: 'foo',
+      })).toEqual({
+        ids: [],
+        isFetching: false,
+        errorMessage: 'foo',
       });
     });
   });
