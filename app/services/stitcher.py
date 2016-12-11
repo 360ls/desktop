a2d35824287d318a7329bdfeb59188ba7d7d0eed
dongy7 <dongy7@gmail.com>
2016-12-10 22:13:22 -0500

    add option to playback video

--- app/services/stitcher.py
+++ app/services/stitcher.py
@@ -28,18 +28,10 @@ def parse_args():
     parser.add_argument('--url', dest='url', default='rtmp://54.227.214.22:1935/live/myStream')
     parser.add_argument('--debug', dest='debug', action='store_true')
     parser.add_argument('--frames', type=int, default=100)
+    parser.add_argument('--playback', dest='playback', action='store_true')
     parser.set_defaults(preview=False)
     parser.set_defaults(stream=False)
     return parser.parse_args()
-
-def check_index(index):
-    """
-    Checks if given index is valid
-    """
-    sample_cap = cv2.VideoCapture(index)
-    frame = sample_cap.grab()
-    sample_cap.release()
-    return frame
 
 def main():
     """
@@ -52,11 +44,15 @@ def main():
     width = args.width
     count = 0
 
-    cap = cv2.VideoCapture(index)
+    if args.playback:
+        cap = cv2.VideoCapture(args.f)
+    else:
+        cap = cv2.VideoCapture(index)
+
     codec = cv2.cv.CV_FOURCC('m', 'p', '4', 'v')
     dimensions = str(width) + 'x' + str(height)
 
-    if args.f:
+    if args.f and not args.playback:
         out = cv2.VideoWriter(dest, codec, 20.0, (width, height))
 
     def handler(signum, frame): # pylint: disable=unused-argument
@@ -65,7 +61,8 @@ def main():
         """
         # When everything done, release the capture
         cap.release()
-        out.release()
+        if args.f and not args.playback:
+            out.release()
         cv2.destroyAllWindows()
         sys.exit(0)
 
@@ -79,22 +76,35 @@ def main():
             'libx264', '-pix_fmt', 'uyvy422', '-r', '28', '-an', '-f', 'flv',
             args.url], stdin=subprocess.PIPE)
 
-    while True:
-        if args.debug and count > args.frames:
-            break
+    if args.playback:
+        playback(cap, args.width, args.height)
+    else:
+        while True:
+            if args.debug and count > args.frames:
+                break
 
-        count += 1
+            count += 1
 
+            # Capture frame-by-frame
+            _, frame = cap.read()
+
+            frame = cv2.resize(frame, (width, height))
+
+            if args.f:
+                out.write(frame)
+
+            if args.stream:
+                proc.stdin.write(frame.tostring())
+
+            # Display the resulting frame
+            cv2.imshow('frame', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+def playback(cap, width, height):
+    while (cap.isOpened()):
         # Capture frame-by-frame
         _, frame = cap.read()
-
-        frame = cv2.resize(frame, (width, height))
-
-        if args.f:
-            out.write(frame)
-
-        if args.stream:
-            proc.stdin.write(frame.tostring())
 
         # Display the resulting frame
         cv2.imshow('frame', frame)
